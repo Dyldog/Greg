3a758a843ffc26443948803893b70268518c9b96
Dylan Elliott <dylan.elliott@gmail.com>
2023-12-22 19:10:46 +1100

    Add bin night demo

--- Sources/Greg/Greg.swift
+++ Sources/Greg/Greg.swift
@@ -2,7 +2,48 @@
 // https://docs.swift.org/swift-book
 
 import Armstrong
+import Alexandria
 
 extension Greg {
-    public static var demoScreens: [Screen] { [] }
+    private static let binNight: Screen = .init(
+        id: .init(),
+        name: "Bin Night",
+        initVariables: .makeDefault(),
+        initActions: .init(value: [
+            GetCurrentDateStep(),
+            GetDateComponentStep(component: .init(value: .weekOfYear), date: .variable(.named("$0"))),
+            IfStep(
+                ifAction: .init(
+                    ifCondition: .init(
+                        lhs: NumericalOperationValue(
+                            lhs: .variable(named: "$0"),
+                            rhs: .int(2),
+                            operation: .init(value: .mod)
+                        ).any,
+                        rhs: .int(0),
+                        comparison: .init(value: .equals)
+                    ),
+                    ifSteps: .init(value: [
+                        SetVarStep(varName: .string("NIGHT"), value: .string("RECYCLING"))
+                    ])
+                ),
+                elseAction: .init(value: [
+                    SetVarStep(varName: .string("NIGHT"), value: .string("GREEN WASTE"))
+                ])
+            )
+        ]),
+        subscreens: [],
+        content: .init([
+            MakeableLabel(
+                id: .init(),
+                text: .variable(named: "NIGHT"),
+                fontSize: .int(36),
+                fontWeight: .init(value: .bold),
+                italic: .false,
+                base: .makeDefault(),
+                textColor: .init(value: .black),
+                isMultiline: .false
+            )
+        ]))
+    public static var demoScreens: [Screen] { [binNight] }
 }
